65a29aed48b54c1ce293fa4d2bf36876f9154c6e
rikuyaosawa <rikuyaosawa@gmail.com>
2024-03-16 15:49:33 +0900

    add csvs for names and sql for creating tables

---
+++ resource_240316/query.sql
@@ -0,0 +1,19 @@
+CREATE TABLE boy_name (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50),
+	meaning VARCHAR(50),
+	nickname VARCHAR(50),
+	origin VARCHAR(50),
+	variation VARCHAR(50),
+	gender VARCHAR(10)
+);
+
+CREATE TABLE girl_name (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50),
+	meaning VARCHAR(50),
+	nickname VARCHAR(50),
+	origin VARCHAR(50),
+	variation VARCHAR(50),
+	gender VARCHAR(10)
+);
